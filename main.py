04afc7105f403b645e2bb01bd732193042f27219
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-06-23 17:09:11 +0200

    push secours

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+from sumolib import checkBinary
+import traci
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+sumoBinary = checkBinary('sumo-gui')
+
+WHITE = [255, 255, 255]
+EDGE_ID = 'closed'
+VEHICLES = ['1', '4', '8']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    startSim()
+
+    while shouldContinueSim():
+        for vehId in getOurDeparted(VEHICLES):
+            setVehColor(vehId, WHITE)
+            avoidEdge(vehId, EDGE_ID)
+        for vehId in traci.vehicle.getIDList():
+            position = traci.vehicle.getPosition(vehId)
+            print("vehicule",vehId,  position[0], position[1],"\n"
+                  )
+        traci.simulationStep()
+    traci.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def startSim():
+    """Starts the simulation."""
+    traci.start(
+        [
+            sumoBinary,
+            '-c', 'network/network.sumocfg',
+            '--delay', '200',
+            '--start'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        ])
+
+
+def shouldContinueSim():
+    """Checks that the simulation should continue running.
+    Returns:
+        bool: `True` if vehicles exist on network. `False` otherwise.
+    """
+    numVehicles = traci.simulation.getMinExpectedNumber()
+    return True if numVehicles > 0 else False
+
+
+def setVehColor(vehId, color):
+    """Changes a vehicle's color.
+    Args:
+        vehId (String): The vehicle to color.
+        color ([Int, Int, Int]): The RGB color to apply.
+    """
+    traci.vehicle.setColor(vehId, color)
+
+
+def avoidEdge(vehId, edgeId):
+    """Sets an edge's travel time for a vehicle infinitely high, and reroutes the vehicle based on travel time.
+    Args:
+        vehId (Str): The ID of the vehicle to reroute.
+        edgeId (Str): The ID of the edge to avoid.
+    """
+    traci.vehicle.setAdaptedTraveltime(
+        vehId, edgeId, float('inf'))
+    traci.vehicle.rerouteTraveltime(vehId)
+
+
+def getOurDeparted(filterIds=[]):
+    """Returns a set of filtered vehicle IDs that departed onto the network during this simulation step.
+    Args:
+        filterIds ([String]): The set of vehicle IDs to filter for.
+    Returns:
+        [String]: A set of vehicle IDs.
+    """
+    newlyDepartedIds = traci.simulation.getDepartedIDList()
+    filteredDepartedIds = newlyDepartedIds if len(
+        filterIds) == 0 else set(newlyDepartedIds).intersection(filterIds)
+    return filteredDepartedIds
+
+
+if __name__ == "__main__":
+    main()
