0e7adebd09547df15ec947a7ffc6b3b1d3793495
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-07-21 11:26:54 +0200

    Finished the display and the error manager

--- api/api.py
+++ api/api.py
@@ -35,39 +35,63 @@ class SumoAPI:
             print("No data available.")
         return tmp
 
-    def get_all_cars(self):
+    def query_builder(self, vehicle=None):
         self.get_list_cars()
         condition = ""
-        for car in self.list:
-            condition = condition + f"""r["vehicle_id"] == "{car}" or """
-        print(condition)
-        result = self.query_api.query(f"""
-                            from(bucket: "db")
-                                  |> range(start: 0)
-                                  |> filter(fn: (r) => r["_measurement"] == "sumo")
-                                  |> filter(fn: (r) => r["_field"] == "co2" or r["_field"] == "latitude" or r["_field"] == "longitude" or r["_field"] == "speed" or  r["_field"] == "simu_id")
-                                  |> group(columns: ["_field",  "vehicle_id"])
+        vehicle_condition = ""
+
+        # build the condition where he displays only vehicle
+        if len(self.list) > 1:
+            condition = "|> filter(fn: (r) => "
+            for i, car in enumerate(self.list):
+                if i == len(self.list) - 1:
+                    condition = condition + f"""r["vehicle_id"] == "{car}" """
+                else:
+                    condition = condition + f"""r["vehicle_id"] == "{car}" or """
+            condition = condition + ")"
+            query = f"""
+                                        from(bucket: "db")
+                                              |> range(start: 0)
+                                              |> filter(fn: (r) => r["_measurement"] == "sumo")
+                                              |> filter(fn: (r) => r["_field"] == "co2" or r["_field"] == "latitude" or r["_field"] == "longitude" or r["_field"] == "speed" or  r["_field"] == "simu_id")
+                                              {condition}
+                                              """
+            print(condition)
+
+        else:
+            return -1
+
+        # build the unique vehicle condition
+        if vehicle is not None:
+            if vehicle in self.list:
+                vehicle_condition = f"""|> filter(fn: (r) => r["vehicle_id"] == "{vehicle}")"""
+                query = query + f"""{vehicle_condition}
+                """
+            else:
+                return -2
+
+        query = query + """       |> group(columns: ["_field",  "vehicle_id"])
                                   |> last()
                                   |> pivot(rowKey:["_time"], columnKey:["_field"], valueColumn:"_value")
                                   |> yield(name: "last_result")
-            """)
-        myjson = result.to_json()
-        return myjson
+            """
+        return query
+
+    def get_car_manager(self, query):
+        if query == -1:
+            return """There is no car in activity"""
+        elif query == -2:
+            return """This car is not car in activity"""
+        else:
+            result = self.query_api.query(query)
+            myjson = result.to_json()
+            return myjson
+
+    def get_all_cars(self):
+        return self.get_car_manager(self.query_builder())
 
     def get_car(self, vehicle):
-        result = self.query_api.query(f"""
-                        from(bucket: "db")
-                              |> range(start: 0)
-                              |> filter(fn: (r) => r["_measurement"] == "sumo")
-                              |> filter(fn: (r) => r["_field"] == "co2" or r["_field"] == "latitude" or r["_field"] == "longitude" or r["_field"] == "speed" or  r["_field"] == "simu_id")
-                              |> group(columns: ["_field",  "vehicle_id"])
-                              |> last()
-                              |> filter(fn: (r) => r["vehicle_id"] == "{vehicle}")
-                              |> pivot(rowKey:["_time"], columnKey:["_field"], valueColumn:"_value")
-                              |> yield(name: "last_result")
-                    """)
-        myjson = result.to_json()
-        return myjson
+        return self.get_car_manager(self.query_builder(vehicle))
 
     def add_car(self, vehicle):
         data_point = Point("toadd") \
@@ -108,14 +132,22 @@ def get_car(vehicle):
 
 @sumo_api.app.route('/cars/<vehicle>/add', methods=['POST'])
 def add_car(vehicle):
-    sumo_api.add_car(vehicle)
-    return f"Car {vehicle} added"
+    sumo_api.get_list_cars()
+    if vehicle in sumo_api.list:
+        return f"Car {vehicle} is in activity"
+    else:
+        sumo_api.add_car(vehicle)
+        return f"Car {vehicle} added"
 
 
 @sumo_api.app.route('/cars/<vehicle>/remove', methods=['DELETE'])
 def remove_car(vehicle):
-    sumo_api.remove_car(vehicle)
-    return f"Car {vehicle} removed"
+    sumo_api.get_list_cars()
+    if vehicle not in sumo_api.list:
+        return f"Car {vehicle} not founded"
+    else:
+        sumo_api.remove_car(vehicle)
+        return f"Car {vehicle} removed"
 
 
 if __name__ == '__main__':
